c56e17ff97758c85a7fc42baa837e1a3707a1fbc
Aline Benvenuto <aline.benvenutto@gmail.com>
2022-01-18 20:29:08 -0300

    Exercício - Cálculo de Área

--- bootcampjava/Main.java
+++ bootcampjava/Main.java
@@ -10,6 +10,17 @@ public class Main {
 		Calculadora.subtracao(10, 5);
 		Calculadora.multiplicacao(10, 2);
 		Calculadora.divisao(20, 10);
+		
+		//Cálculo da Área
+		System.out.print("\nExecício - Cáculo das ÁREAS");
+		double areaQuadrado = CalculoArea.area(3);
+		System.out.print("\n- Àrea do quadrado: "+ areaQuadrado);
+		
+		double areaRetangulo = CalculoArea.area(2, 2);
+		System.out.print("\n- Àrea do triângulo: "+ areaRetangulo);
+		
+		double areaTrapezio = CalculoArea.area(10, 5, 2);
+		System.out.print("\n- Àrea do trapézio: "+ areaTrapezio);
 	}
 
 }
